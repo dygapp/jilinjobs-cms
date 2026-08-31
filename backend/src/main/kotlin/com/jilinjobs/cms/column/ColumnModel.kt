c884b01303150ac70204cbcb43e736bdf89a0470
dygapp <dygapp@qq.com>
2026-08-31 17:08:01 +0800

    feat(cms): 栏目增加文章封面策略

--- backend/src/main/kotlin/com/jilinjobs/cms/column/ColumnModel.kt
+++ backend/src/main/kotlin/com/jilinjobs/cms/column/ColumnModel.kt
@@ -1,5 +1,7 @@
 package com.jilinjobs.cms.column
 
+import com.jilinjobs.cms.common.ContentImagePolicy
+
 data class CmsColumn(
     val id: Long,
     val parentId: Long?,
@@ -7,6 +9,7 @@ data class CmsColumn(
     val sortOrder: Int,
     val enabled: Boolean,
     val alias: String = "",
+    val coverPolicy: ContentImagePolicy = ContentImagePolicy.OPTIONAL,
 )
 
 data class ColumnDraft(
@@ -15,6 +18,7 @@ data class ColumnDraft(
     val sortOrder: Int,
     val enabled: Boolean,
     val alias: String = "",
+    val coverPolicy: ContentImagePolicy = ContentImagePolicy.OPTIONAL,
 )
 
 interface ColumnQuery {
