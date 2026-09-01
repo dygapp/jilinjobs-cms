9baa13017b3976772c9e7c6ce933987de6577cf8
dygapp <dygapp@qq.com>
2026-09-01 23:07:58 +0800

    refactor(public): 收敛主站模块化与单入口 (#36)

---
+++ frontend/public-site/src/sites/main/components/PublicSiteFooter.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import PublicSiteFooter from '../shell/PublicSiteFooter.vue'
+</script>
+
+<template>
+  <PublicSiteFooter />
+</template>
---
+++ frontend/public-site/src/sites/main/components/PublicSiteHeader.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import PublicSiteHeader from '../shell/PublicSiteHeader.vue'
+</script>
+
+<template>
+  <PublicSiteHeader />
+</template>
