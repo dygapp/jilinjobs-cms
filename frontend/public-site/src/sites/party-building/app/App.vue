faa49a201435a018bf9757d4ec21636c0cb01a89
dygapp <dygapp@qq.com>
2026-09-03 08:46:12 +0800

    feat(party): 提升党建 Shell 生命周期

--- frontend/public-site/src/sites/party-building/app/App.vue
+++ frontend/public-site/src/sites/party-building/app/App.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+import PartyBuildingHeader from '../shell/PartyBuildingHeader.vue'
+import PartyBuildingFooter from '../shell/PartyBuildingFooter.vue'
+</script>
+
 <template>
-  <router-view />
+  <div class="party-site-app" data-testid="party-building-site">
+    <PartyBuildingHeader />
+    <router-view />
+    <PartyBuildingFooter />
+  </div>
 </template>
