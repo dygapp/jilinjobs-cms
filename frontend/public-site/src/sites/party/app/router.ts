9342fe58126fb008db698d88f636106080eb313b
dygapp <dygapp@qq.com>
2026-09-03 18:12:38 +0800

    fix(party): 修正 PartyHome 路由组件路径

--- frontend/public-site/src/sites/party/app/router.ts
+++ frontend/public-site/src/sites/party/app/router.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 export default createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/party/', name: 'party-home', component: () => import('../modules/entry/PartyHomeView.vue') },
+    { path: '/party/', name: 'party-home', component: () => import('../modules/home/PartyHomeView.vue') },
     { path: '/party/column/:alias', name: 'party-column', component: () => import('../modules/content/PartyColumnView.vue') },
     { path: '/party/article/:id', name: 'party-article', component: () => import('../modules/content/PartyArticleView.vue') },
     { path: '/party/:pathMatch(.*)*', redirect: '/party/' },
