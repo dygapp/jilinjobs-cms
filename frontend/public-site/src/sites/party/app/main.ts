109cd5ae36f4ec66e18058a13f0a95f59f7657e7
dygapp <dygapp@qq.com>
2026-09-04 13:36:02 +0800

    fix(party): 统一栏目交互主题色

--- frontend/public-site/src/sites/party/app/main.ts
+++ frontend/public-site/src/sites/party/app/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '../styles/party.css'
+import '../styles/party-column-theme.css'
 
 createApp(App).use(router).mount('#app')
