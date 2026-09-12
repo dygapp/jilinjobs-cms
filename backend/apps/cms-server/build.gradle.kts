7f73f0eb9acf0842f239664941b3651c4e72e83d
dygapp <dygapp@qq.com>
2026-09-12 16:26:22 +0800

    fix: add Jackson Kotlin support for Structured Pages

--- backend/apps/cms-server/build.gradle.kts
+++ backend/apps/cms-server/build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("tools.jackson.module:jackson-module-kotlin")
 
     runtimeOnly("org.flywaydb:flyway-mysql")
     runtimeOnly("com.mysql:mysql-connector-j")
